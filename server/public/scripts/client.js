8d4f7460b9b0ea60519df81d0b2561f106d4e65e
Kenneth Carter <kxccarter@gmail.com>
2020-07-09 15:24:31 -0500

    added in Bootstrap because i can't think when it's ugly.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,62 +1,64 @@
 $(document).ready(handleReady);
 
 function handleReady() {
-  // Set up click listeners
-  $('#submit').on('click', handleSubmit);
+    // Set up click listeners
+    $('#submit').on('click', handleSubmit);
 
+    // DELETE button event listener
+    // $('#jsShoeDisplay').on('click', '.js-item-ctrl', deleteItem);
 
-  // Get data
-  getShoes();
+    // Get data
+    getShoes();
 }
 
-function handleSubmit() { 
-  const name = $('#name').val();
-  const size = $('#size').val();
-  const cost = $('#cost').val();
+function handleSubmit() {
+    const name = $('#name').val();
+    const size = $('#size').val();
+    const cost = $('#cost').val();
 
-  const objectToSend = {
-    name : name,
-    size : size,
-    cost : cost
-  }
+    const objectToSend = {
+        name: name,
+        size: size,
+        cost: cost
+    }
 
-  $.ajax({
-    method: 'POST',
-    url: '/shoes',
-    data : objectToSend
-  }).then( function(response ) {
-    //database is updated, need to update DOM
-    getShoes();
-  }).catch(function(err){
-    console.log(err);
-    alert('Something went wrong in POST');
-  })
+    $.ajax({
+        method: 'POST',
+        url: '/shoes',
+        data: objectToSend
+    }).then(function(response) {
+        //database is updated, need to update DOM
+        getShoes();
+    }).catch(function(err) {
+        console.log(err);
+        alert('Something went wrong in POST');
+    })
 
- }
+}
 
 function getShoes() {
-  $.ajax({
-      method: 'GET',
-      url: '/shoes'
-  }).then(function(response) {
-      console.log('getShoes - response', response);
+    $.ajax({
+        method: 'GET',
+        url: '/shoes'
+    }).then(function(response) {
+        console.log('getShoes - response', response);
 
-      //Append Shoes
-      appendShoeTable(response);
-  }).catch(function(error){
-      console.log('getShoes - error', error);
-      alert('something went wrong in GET')
-  })
+        //Append Shoes
+        appendShoeTable(response);
+    }).catch(function(error) {
+        console.log('getShoes - error', error);
+        alert('something went wrong in GET')
+    })
 }
 
 function appendShoeTable(shoes) {
-  //empty old data
-  $('#shoeDisplay').empty();
+    //empty old data
+    $('#jsShoeDisplay').empty();
 
-  for (let shoe of shoes) {
-    console.log(shoe);
-    //make a row with info
-    let tableRow = $(`
+    for (let shoe of shoes) {
+        console.log(shoe);
+        //make a row with info
+        let tableRow = $(`
     <tr>
       <td>${shoe.name}</td>
       <td>${shoe.cost}</td>
@@ -64,10 +66,10 @@ function appendShoeTable(shoes) {
     </tr>
     `)
 
-    // attach data to row, need for delete
-    tableRow.data('id', shoe.id);
+        // attach data to row, need for delete
+        tableRow.data('id', shoe.id);
 
-    //append row to table
-    $('#shoeDisplay').append(tableRow);
-  }
+        //append row to table
+        $('#jsShoeDisplay').append(tableRow);
+    }
 }
